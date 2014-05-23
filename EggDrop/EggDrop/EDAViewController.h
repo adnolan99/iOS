7aafde72f93417b97ecaa543b9b44fd1ed7404cf
adnolan99 <adnolan99@gmail.com>
2014-05-23 09:31:40 -0400

    May 23, friday morning

---
+++ EggDrop/EggDrop/EDAViewController.h
@@ -0,0 +1,23 @@
+//
+//  EDAViewController.h
+//  EggDrop
+//
+//  Created by Austin Nolan on 5/22/14.
+//  Copyright (c) 2014 Austin Nolan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EDAViewController : UIViewController
+
+
+
+
+@property (weak, nonatomic) IBOutlet UIImageView *spoon;
+
+@property (weak, nonatomic) IBOutlet UIImageView *egg;
+
+
+
+
+@end
